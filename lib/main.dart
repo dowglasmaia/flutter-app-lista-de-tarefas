2c9b3c19951b9f5c1dfa3fc26fa8d5912a147faf
DOWGLAS <dowglasmaia@live.com>
2020-09-24 16:13:50 -0300

    impl metodo add na lista e mudando estado do check

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,20 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _toDoList = ["Dowglas", "Kayron"];
+  final _toDoController = TextEditingController();
+
+  List _toDoList = [];
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+
+      _toDoList.add(newToDo);
+    });
+  }
 
   Future<File> _getFile() async {
     final pathDirectory = await getApplicationDocumentsDirectory();
@@ -59,13 +72,14 @@ class _HomeState extends State<Home> {
                     decoration: InputDecoration(
                         labelText: "Nova Tarefa",
                         labelStyle: TextStyle(color: color)),
+                    controller: _toDoController,
                   ),
                 ),
                 RaisedButton(
                   color: color,
                   child: Text("ADD"),
                   textColor: Colors.white,
-                  onPressed: () {},
+                  onPressed: _addToDo,
                 )
               ],
             ),
@@ -81,9 +95,15 @@ class _HomeState extends State<Home> {
                     title: Text(_toDoList[index]["title"]),
                     value: _toDoList[index]["ok"],
                     secondary: CircleAvatar(
-                      child:
-                      Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+                      child: Icon(
+                          _toDoList[index]["ok"] ? Icons.check : Icons.error),
                     ),
+                    onChanged: (c) {
+                      //Atualiza o estado da operação
+                      setState(() {
+                        _toDoList[index]["ok"] = c;
+                      });
+                    },
                   );
                 }),
           ),
